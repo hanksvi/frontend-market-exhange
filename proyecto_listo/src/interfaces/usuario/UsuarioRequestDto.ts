1d41f3d8f6bcc7b41c6e4ba02827e39c25d10c41
AnthRom <luis.romero@utec.edu.pe>
2024-11-24 04:00:08 -0500

    cambios-realizados

--- proyecto_listo/src/interfaces/usuario/UsuarioRequestDto.ts
+++ proyecto_listo/src/interfaces/usuario/UsuarioRequestDto.ts
@@ -1,14 +1,5 @@
 export interface UsuarioRequestDto {
-<<<<<<< HEAD
-    firstname: string; // El nombre del usuario, obligatorio, máximo 50 caracteres
-    lastname: string; // El apellido del usuario, obligatorio, máximo 50 caracteres
-    email: string; // El correo electrónico del usuario, obligatorio y válido
-    phone: string; // El teléfono del usuario, obligatorio, entre 7 y 15 caracteres
-    password: string; // La contraseña del usuario, obligatoria, mínimo 8 caracteres
-    address: string; // La dirección del usuario, obligatoria, máximo 100 caracteres
-    role: string; // El rol del usuario, obligatorio
-  }
-=======
+
   firstname: string; // El nombre del usuario, obligatorio, máximo 50 caracteres
   lastname: string; // El apellido del usuario, obligatorio, máximo 50 caracteres
   email: string; // El correo electrónico del usuario, obligatorio y válido
@@ -17,4 +8,4 @@ export interface UsuarioRequestDto {
   address: string; // La dirección del usuario, obligatoria, máximo 100 caracteres
   role: string; // El rol del usuario, obligatorio
 }
->>>>>>> e0ef68c0bff4ee68745072a8309e797ec369c6fe
+
